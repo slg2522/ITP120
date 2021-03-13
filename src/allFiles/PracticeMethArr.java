b04d08cb179ae8e30297cb04bbebfa4d9e808bb8
slg2522 <slg2522@email.vccs.edu>
2021-03-13 00:25:40 -0500

    Fixed Accumulator for Colors
    Repetitions and tally are not doubles to match average.

--- src/allFiles/PracticeMethArr.java
+++ src/allFiles/PracticeMethArr.java
@@ -1,25 +1,96 @@
 package allFiles;
 
+/**Practice Methods & Arrays Problems for Exam 2
+ * 
+ * @author Sabrina Groves
+ * @version 1.0
+ * @since 3/12/2021
+ */
+
+import java.util.Scanner;
+
 public class PracticeMethArr
 {
+  //opens scanner
+  private static Scanner scan = new Scanner(System.in);
+  
 	public static void main(String[] args)
 	{
-		/**In physics, an object that is in motion is said to have
-		 * kinetic energy. The following formula can be used to
-		 * determine a moving object's kinetic energy:
-		 * KE = 1/2 mv^2
-		 * where KE = kinetic energy
-		 * m = object mass (kilograms)
-		 * v = velocity (m/s)
-		 * 
-		 * Write a method named kineticEnergy that accepts an
-		 * object's mass (in kilograms) and velocity (in meters
-		 * per second) as arguments. The method should return the
-		 * amount of kinetic energy that the object has.
-		 * Demonstrate the method by calling it in a program that
-		 * asks the user to enter values for mass and velocity.*/
+		
+		/**KINETIC ENERGY CALL*/
+		//program purpose
+		System.out.println("This program takes in an object's mass and velocity, and outputs its kinetic energy. ");
+		
+		//takes variable input
+		System.out.println("Enter the object's mass:  ");
+		double m = scan.nextDouble();
+		System.out.println("Enter the object's velocity:  ");
+		double v = scan.nextDouble();
+		double ke = kineticEnergy(m, v);
+		
+		//formats kinetic energy with comma and 2 decimal places
+		System.out.print("The object's kinetic energy is ");
+		System.out.printf("%,.2f", ke);
+		System.out.println(" joules.");
+		
+		//_____________________________________
+		
+		/**EXTRASENSORY PERCEPTION CALL*/
+		//Average Correct
+		//String userColor = pickColor();
+		//String randomColor = randColor();
+		Double accuracy = guessAccuracy();
+		System.out.println("The accuracy of your guesses is: " + accuracy + "%.");
+		
+		//_____________________________________
+		
+		/**RAINFALL CALL*/
 		
 		
+		//_____________________________________
+		
+		/**GREATER THAN CALL*/
+		
+		
+		//_____________________________________
+		
+		/**STOCK SALE CALL*/
+		
+		
+		//_____________________________________
+		
+		//closes scanner
+		scan.close();
+	}
+	
+	//___________________________________________________________________________
+	
+	/**In physics, an object that is in motion is said to have
+	 * kinetic energy. The following formula can be used to
+	 * determine a moving object's kinetic energy:
+	 * KE = 1/2 mv^2
+	 * where KE = kinetic energy
+	 * m = object mass (kilograms)
+	 * v = velocity (m/s)
+	 * 
+	 * Write a method named kineticEnergy that accepts an
+	 * object's mass (in kilograms) and velocity (in meters
+	 * per second) as arguments. The method should return the
+	 * amount of kinetic energy that the object has.
+	 * Demonstrate the method by calling it in a program that
+	 * asks the user to enter values for mass and velocity.*/
+	
+	//@param takes mass and velocity as inputs (double)
+	public static double kineticEnergy(double mass, double velocity)
+	{
+		//squares the velocity
+		double velsquare = Math.pow(velocity, 2);
+		
+		//computes kinetic energy
+		double kenergy = 0.5 * mass * velsquare;
+		
+		//@return kinetic energy
+		return kenergy;
 	}
 	
 	//___________________________________________________________________________	
@@ -39,6 +110,109 @@ public class PracticeMethArr
 	 *  number of times the user correctly guessed the selected color.
 	 *  Be sure to modularize the program into methods that perform each task.*/
 	
+	//takes the user's color input
+	public static String pickColor()
+	{	
+		//take string color input from the user
+		System.out.println("Pick a color from the list (red, green, blue, orange, yellow): ");
+		String userColor = scan.next();
+		
+		//@return user's color choice
+		return userColor;
+	}
+	
+	//computer generates a random color
+	public static String randColor()
+	{
+		//generate a random number
+		double randomNumber = Math.random();
+		
+		//raise the range to between 0 and 4 (the elements of the array)
+		randomNumber = randomNumber * 4;
+		
+		//casts the random number to an integer
+		int randomInteger = (int) randomNumber;
+		
+		//uses the random integer to select an element from a list
+		//@return corresponding color as a string
+		String randomColor[] = {"Red", "Green", "Blue", "Orange", "Yellow"};
+		String compColor = "blank";
+		switch(randomInteger)
+		{
+		case 0:
+			compColor = randomColor[0];
+			break;
+		case 1:
+			compColor = randomColor[1];
+			break;
+		case 2:
+			compColor = randomColor[2];
+			break;
+		case 3:
+			compColor = randomColor[3];
+			break;
+		case 4:
+			compColor = randomColor[4];
+				break;
+		}
+      	return compColor;    //ERROR HERE!! When I put it here, it asks me to declare compColor??
+      
+	}
+	
+	//compares the user guess to the computer color 10 times & computes accuracy
+	public static double guessAccuracy()
+	{
+		//to loop 10 times
+		double repetitions = 10;
+		
+		//accumulator and array declaration
+      	double tally=0;
+		String[] userColorList = new String[10];
+		String[] computerColorList = new String[10];
+      	
+      	//test returns 1.0
+      	//String[] userColorList = {"red", "red", "red", "red", "red", "red", "red", "red", "red", "red"};
+      	//String[] computerColorList = {"red", "red", "red", "red", "red", "red", "red", "red", "red", "red"};
+      
+		for(int index = 0; index < repetitions; index++)
+		{
+			//define arrays to hold colors
+			userColorList[index] = pickColor();
+			//check what's being added
+			//System.out.println(userColorList[index]);
+			computerColorList[index] = randColor();
+			//check what's being added
+			//System.out.println(computerColorList[index]);
+          	
+			//compare individual colors
+			String userColor = userColorList[index];
+          	String computerColor = computerColorList[index];
+          	
+          	//if they match, award a point
+          	if (userColor.equalsIgnoreCase(computerColor))
+          	{
+          		tally += 1;
+          		//check status
+          		System.out.println("Match!");
+          	}
+          	else
+          	{
+          			//check status
+          			System.out.println("No match.");
+          	}
+		}
+		//check that these are working
+		//System.out.println("Tally:" + tally);
+        //System.out.println("Repetitions:" + repetitions);
+		
+        //get average correct
+        double average = (tally/repetitions) * 100;
+		
+        //@return average correct color guesses as a percent
+		return average;
+		
+		}
+	}
 	
 	//___________________________________________________________________________
 	
@@ -76,5 +250,3 @@ public class PracticeMethArr
 	 * Demonstrate the method in a program that asks the user to enter the necessary data
 	 * and displays the amount of the profit or loss.*/
 	
-	
-}
