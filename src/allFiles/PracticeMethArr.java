960d2b4b7c6b1d3ab3774523449a4a6fbc1d6b3f
slg2522 <slg2522@email.vccs.edu>
2021-03-13 17:25:12 -0500

    Finished Methods & Arrays Practice Problems
    Adds ArrayList and Stocks Calculations

--- src/allFiles/PracticeMethArr.java
+++ src/allFiles/PracticeMethArr.java
@@ -3,11 +3,11 @@ package allFiles;
 /**Practice Methods & Arrays Problems for Exam 2
  * 
  * @author Sabrina Groves
- * @version 1.0
- * @since 3/12/2021
+ * @version 2.0
+ * @since 3/13/2021
  */
-
-import java.util.Scanner;
+//import java.util.Arrays;
+import java.util.*;
 
 public class PracticeMethArr
 {
@@ -33,30 +33,73 @@ public class PracticeMethArr
 		System.out.printf("%,.2f", ke);
 		System.out.println(" joules.");
 		
+		//separate the problems
+		System.out.println("");
 		//_____________________________________
 		
 		/**EXTRASENSORY PERCEPTION CALL*/
 		//Average Correct
-		//String userColor = pickColor();
-		//String randomColor = randColor();
 		Double accuracy = guessAccuracy();
 		System.out.println("The accuracy of your guesses is: " + accuracy + "%.");
 		
+		//separate the problems
+		System.out.println("");
 		//_____________________________________
 		
 		/**RAINFALL CALL*/
+		//List of rain by month
+		double[] rainarray = new double[12];
+		rainarray = RainFall.monthlyRain();
+		System.out.println("The monthly rainfall is as follows: " + Arrays.toString(rainarray));
 		
+		//total rainfall in a year
+		double yearlyRain = RainFall.totalRain(rainarray);
+		System.out.print("The yearly total rainfall is: ");
+		System.out.printf("%,.2f", yearlyRain);
+		System.out.println(" units.");
 		
+		//average monthly rainfall
+		double average = RainFall.averageRain(yearlyRain);
+		System.out.print("The average monthly rainfall is: ");
+		System.out.printf("%,.2f", average);
+		System.out.println(" units.");
+		
+		//max monthly rainfall
+		double max = RainFall.mostRain(rainarray);
+		String highMonth = RainFall.maxMonth(rainarray, max);
+		System.out.print("The maximum rainfall was ");
+		System.out.printf("%,.2f", max);
+		System.out.println(" units, and occurred in " + highMonth + ".");
+		
+		//min monthly rainfall
+		double min = RainFall.leastRain(rainarray);
+		String lowMonth = RainFall.minMonth(rainarray, min);
+		System.out.print("The minimum rainfall was ");
+		System.out.printf("%,.2f", min);
+		System.out.println(" units, and occurred in " + lowMonth + ".");
+		
+		//separate the problems
+		System.out.println("");
 		//_____________________________________
 		
 		/**GREATER THAN CALL*/
+		ArrayList<Integer> greaterarray = new ArrayList<Integer>();
+		greaterarray = GreaterThan.whosGreater();
+		System.out.println("The numbers greater than your integer are: " + greaterarray);
 		
-		
+		//separate the problems
+		System.out.println("");
 		//_____________________________________
 		
 		/**STOCK SALE CALL*/
+		double money = stockProfits.stockOutcome();
+		String gainorloss = stockProfits.gainLoss(money);
+		System.out.print("Your capital " + gainorloss + " were $");
+		System.out.printf("%,.2f", money);
+		System.out.println(".");
 		
-		
+		//separate the problems
+		System.out.println("");
 		//_____________________________________
 		
 		//closes scanner
@@ -153,10 +196,9 @@ public class PracticeMethArr
 			break;
 		case 4:
 			compColor = randomColor[4];
-				break;
+			break;
 		}
-      	return compColor;    //ERROR HERE!! When I put it here, it asks me to declare compColor??
-      
+      	return compColor;
 	}
 	
 	//compares the user guess to the computer color 10 times & computes accuracy
@@ -210,8 +252,6 @@ public class PracticeMethArr
 		
         //@return average correct color guesses as a percent
 		return average;
-		
-		}
 	}
 	
 	//___________________________________________________________________________
@@ -226,27 +266,337 @@ public class PracticeMethArr
 	 * Demonstrate the class in a complete program.
 	 * Input Validation: Do not accept negative numbers for monthly rainfall figures.*/
 	
+	//declares new class for rainfall
+	static class RainFall
+	{
+		//open new scanner for RainFall class
+		private static Scanner rainscan = new Scanner (System.in);     //WHERE DO I CLOSE THIS????????????????????????????
+		
+		//method takes monthly input from the user
+		public static double[] monthlyRain()
+		{
+		System.out.println("This program takes the monthly rainfall for the most recent year. Please enter the rainfall for");
+		
+		//12 months in year shouldn't change
+		final int months = 12;
+		
+		//new array to hold rainfall by month
+		double[] rainlist = new double[12];
+		
+		//loop to take input from user one month at a time
+		for(int index = 0; index < months; index++)
+		{
+			System.out.println("Month " + (index+1) + ":  ");
+			rainlist[index] = rainscan.nextDouble();
+			
+			//don't allow inputs less than zero
+			if (rainlist[index] >= 0)
+			{
+				System.out.println("Thank you for your input.");
+			}
+			else
+			{
+				System.out.println("Rainfall must be above 0 units. Enter a valid rainfall:  ");
+				rainlist[index] = rainscan.nextDouble();
+			}
+		}
+		//@return rainlist: array list of rainfall by month
+		return rainlist;
+		}
+		
+		//method finds total rainfall in one year
+		//@param rainlist: takes array of rainfall for 12 months
+		public static double totalRain(double[] rainlist)
+		{
+			//accumulator
+			double total = 0;
+			
+			//adds one month at a time to total
+			for(int index = 0; index < rainlist.length; index++)
+			{
+				total = total + rainlist[index];
+			}
+		//@return total: gives total yearly rainfall
+		return total;
+		}
+		
+		//method finds the average monthly rainfall
+		//@param total: takes total yearly rainfall
+		public static double averageRain(double total)
+		{
+			final int monthsinyear = 12;
+			double avgrain = total/monthsinyear;
+			
+			//@return avgrain: gives average monthly rainfall
+			return avgrain;
+		}
+		
+		//method finds max rain in a month
+		public static double mostRain(double[] rainlist)
+		{
+			//sorts from lowest to highest
+			Arrays.sort(rainlist);
+			return rainlist[rainlist.length - 1];
+		}
+		
+		//method finds month with most rain
+		public static String maxMonth(double[] rainlist, double maximum)
+		{
+			//sorts rainlist[] and finds max rain
+			Arrays.sort(rainlist);
+			double max = rainlist[(rainlist.length - 1)];
+			
+			//defines month outside the loop
+			String month = "none";
+			
+			//matches max rain to max rain in month order
+			for(int index = 0; index < rainlist.length; index++)
+			{
+				if(max == rainlist[index])
+				{
+					int monthNum = index;
+					switch(monthNum)
+					{
+					case 0:
+						month = "January";
+						break;
+					case 1:
+						month = "February";
+						break;
+					case 2:
+						month = "March";
+						break;
+					case 3:
+						month = "April";
+						break;
+					case 4:
+						month = "May";
+						break;
+					case 5:
+						month = "June";
+						break;
+					case 6:
+						month = "July";
+						break;
+					case 7:
+						month = "August";
+						break;
+					case 8:
+						month = "September";
+						break;
+					case 9:
+						month = "October";
+						break;
+					case 10:
+						month = "November";
+						break;
+					case 11:
+						month = "December";
+						break;
+					}
+				}
+			}
+			//@return month: gives month, as string, with highest rainfall
+			return month;
+		}
+		
+				//method finds min rain in a month
+				public static double leastRain(double[] rainlist)
+				{
+					Arrays.sort(rainlist);
+					return rainlist[0];
+				}
+				
+				//method finds month with most rain
+				public static String minMonth(double[] rainlist, double minimum)
+				{
+					//sorts rainlist[] and finds max rain
+					Arrays.sort(rainlist);
+					double min = rainlist[0];
+					
+					//defines month outside the loop
+					String month = "none";
+					
+					//matches max rain to max rain in month order
+					for(int index = 0; index < rainlist.length; index++)
+					{
+						if(min == rainlist[index])
+						{
+							int monthNum = index;
+							switch(monthNum)
+							{
+							case 0:
+								month = "January";
+								break;
+							case 1:
+								month = "February";
+								break;
+							case 2:
+								month = "March";
+								break;
+							case 3:
+								month = "April";
+								break;
+							case 4:
+								month = "May";
+								break;
+							case 5:
+								month = "June";
+								break;
+							case 6:
+								month = "July";
+								break;
+							case 7:
+								month = "August";
+								break;
+							case 8:
+								month = "September";
+								break;
+							case 9:
+								month = "October";
+								break;
+							case 10:
+								month = "November";
+								break;
+							case 11:
+								month = "December";
+								break;
+							}
+						}
+					}
+					//@return month: gives string for low rainfall month
+					return month;
+				}
+	}
 	
 	//___________________________________________________________________________
 	
-	/**In a program, write a method that accepts two arguments: an array and a number n.
-	 * Assume that the array contains integers. The method should display all of the
-	 * numbers in the array that are greater than the number n.*/
+		/**In a program, write a method that accepts two arguments: an array and a number n.
+		 * Assume that the array contains integers. The method should display all of the
+		 * numbers in the array that are greater than the number n.*/
 	
+	//Creates a class to compare an array and number to see which is greater
+	static class GreaterThan
+	{
+		//Creates a scanner
+		private static Scanner greaterscan = new Scanner (System.in);
+		
+		public static ArrayList<Integer> whosGreater()
+		{
+			//declares an ArrayList with indeterminate length (not possible in an array)
+			ArrayList<Integer> arrayofintegers = new ArrayList<Integer>();
+			
+			//takes an integer from the user
+			System.out.println("Enter a single integer:  ");
+			int userintegerinput = greaterscan.nextInt();
+			
+			//sets up variables to continue if yes
+			String input;
+			String y = "yes";
+			
+			//do while loop to fill the array with user input (continues while "yes")
+			do
+			{
+				//fills the array
+				System.out.println("Enter an integer to fill the array:  ");
+				arrayofintegers.add(greaterscan.nextInt());
+				
+				//continue filling or stop
+				System.out.println("Would you like to enter another integer?");
+				input = scan.next();
+			}
+			while(input.equalsIgnoreCase(y));
+			
+			//finds size of the ArrayList
+			int size = arrayofintegers.size();
+			
+			//create an array to hold greater numbers
+			ArrayList<Integer> greaternumbers = new ArrayList<Integer>();
+			
+			//check which arraylist numbers are greater than the single integer and adds them to a new arraylist
+			for(int count = 0; count < size; count++)
+			{
+				if(arrayofintegers.get(count) > userintegerinput)
+				{
+					greaternumbers.add(arrayofintegers.get(count));
+				}
+			}
+			//@return greaternumbers: gives arraylist of numbers in array greater than the user's integer
+			return greaternumbers;	
+		}
+	}
+		
+		//___________________________________________________________________________
+		
+		/**The profit from the sale of a stock can be calculated as follows:
+		 * 			Profit=((NS * SP)-SC) - ((NS * PP) + PC)
+		 * where NS is the number of shares, PP is the purchase price per share, PC is the
+		 * per-share commission paid, SP is the sale price per share, and SC is the sale
+		 * commission paid. If the calculation yields a positive value, then the
+		 * sale of the stock resulted in a profit. If the calculation yields a negative
+		 * number, then the sale resulted in a loss.
+		 * 
+		 * Write a method that accepts, as arguments, the number of shares, the purchase price
+		 * per share, the purchase commission paid, the sale per share, the sale commission
+		 * paid. The method should return the profit (or loss) from the sale of the stock.
+		 * Demonstrate the method in a program that asks the user to enter the necessary data
+		 * and displays the amount of the profit or loss.*/
 	
-	//___________________________________________________________________________
-	
-	/**The profit from the sale of a stock can be calculated as follows:
-	 * 			Profit=((NS * SP)-SC) - ((NS * PP) + PC)
-	 * where NS is the number of shares, PP is the purchase price per share, PC is the
-	 * purchare commission paid, SP is the sale price per share, and SC is the sale
-	 * commission paid. If the calculation yields a positive value, then the
-	 * sale of the stock resulted in a profit. If the calculation yields a negative
-	 * number, then the sale resulted in a loss.
-	 * 
-	 * Write a method that accepts, as arguments, the number of shares, the purchase price
-	 * per share, the purchase commission paid, the sale per share, the sale commission
-	 * paid. The method should return the profit (or loss) from the sale of the stock.
-	 * Demonstrate the method in a program that asks the user to enter the necessary data
-	 * and displays the amount of the profit or loss.*/
-	
+	//creates a class to handle stock sales
+	static class stockProfits
+	{
+		//creates a scanner
+		private static Scanner stockscan = new Scanner(System.in);
+		
+		//method to determine profit or loss
+		public static double stockOutcome()
+		{
+			//get number of shares
+			System.out.println("How many stock shares did you have? ");
+			double NS = stockscan.nextDouble();
+			
+			//get purchase price per share
+			System.out.println("What was the purchase price per share? ");
+			double PP = stockscan.nextDouble();
+			
+			//get per-share commission paid
+			System.out.println("What did you pay in per-share commission? ");
+			double PC = stockscan.nextDouble();
+			
+			//get sale price per share
+			System.out.println("What was the sale price per share? ");
+			double SP = stockscan.nextDouble();
+			
+			//get sale commission paid
+			System.out.println("How much did you pay in sales commission? ");
+			double SC = stockscan.nextDouble();
+			
+			//declares and sets gains/losses equal to formula
+			double stockresult = ((NS * SP) - SC) - ((NS * PP) + PC);
+			
+			//@return stockresult: gives money gained/lost from the market
+			return stockresult;
+		}
+		
+		//method to determine gain, neutral, or loss
+		//@param stockresult: takes monetary outcome from the market
+		public static String gainLoss(double stockresult)
+		{
+			//creates variable to hold gain/loss
+			String result;
+			
+			//determines if gain or loss from market
+			if(stockresult >= 0)
+			{
+				result = "gains";
+			}
+			else
+			{
+				result = "losses";
+			}
+		
+		//@return result: gives string gain or loss
+		return result;
+		}
+	}
+}
